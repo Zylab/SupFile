defff564dee847d78256dee9033608dcaa065a31
SILIGHINI Hugo <205528@supinfo.com>
2018-02-16 18:46:42 +0100

    Initial commit

---
+++ app/src/main/java/com/supfile/supfile/SupFile.java
@@ -0,0 +1,13 @@
+package com.supfile.supfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupFile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sup_file);
+    }
+}
